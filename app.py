49c9b794957dfc0f43e3ca3d5544e1a82ffebf90
Iira <iira.hakkinen@helsinki.fi>
2021-04-01 16:29:13 +0300

    kirjautumisen aloitus

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/mainpage", methods=["POST"])
+def mainpage():
+    return render_template("mainpage.html",name=request.form["name"])
