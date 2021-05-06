06eb631b90cc02dc2a1db6ffdb30c0747cc2e210
Iira <iira.hakkinen@helsinki.fi>
2021-05-06 13:28:23 +0300

    tietokantataulut

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (id SERIAL PRIMARY KEY, name TEXT, username TEXT, password TEXT, admin BOOLEAN);
-CREATE TABLE destinations (id SERIAL PRIMARY KEY, name TEXT, town TEXT, ranking INT);
-CREATE TABLE reviews (id SERIAL PRIMARY KEY, destinationId FOREIGN KEY, username TEXT, ranking INT, comment TEXT);
+CREATE TABLE destinations (id SERIAL PRIMARY KEY, name TEXT, town TEXT);
+CREATE TABLE reviews (id SERIAL PRIMARY KEY, destinationId INT, userId INT, ranking INT, comment TEXT);
 CREATE TABLE attractions (id SERIAL PRIMARY KEY, destinationId INT, name TEXT, info TEXT);
 CREATE TABLE favourites (id SERIAL PRIMARY KEY, userId INT, destinationId INT);
